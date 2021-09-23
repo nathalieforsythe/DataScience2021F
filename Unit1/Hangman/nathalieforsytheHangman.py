e0c4763f663612543ac0556123f50314e4b824ed
nathalieforsythe <nforsythe22@penguinhall.org>
2021-09-22 21:11:47 -0400

    Correct print statements

--- Unit1/Hangman/nathalieforsytheHangman.py
+++ Unit1/Hangman/nathalieforsytheHangman.py
@@ -1,4 +1,4 @@
-countries = ['Canada', 'Mexico', 'France']
+countries = ['canada', 'mexico', 'france'] # strip, lower? (in pickWord)
 guessedLetters = []
 guessesLeft = 10
 
@@ -7,11 +7,17 @@ def runGame():
     welcome(country)
     while guessesLeft > 0: # or entire word is guessed
         guess = enterGuess()
-        checkGuess(guess, country)
-        # end when word is guessed/guesses run out
+        if guess in guessedLetters:
+            print('You already guessed that.')
+        elif guess not in guessedLetters:
+            checkForCorrectGuess(guess, country)
+        if guess not in country:
+            print('Your guess is incorrect.') 
+        updateGuesses(guess)
+        # end when word is guessed/guesses run out - stops when guesses run out
 
 def pickWord():
-    country = countries[1] # figure out how to pick randomly
+    country = countries[0] # figure out how to pick randomly
     return country
 
 def welcome(country):
@@ -20,20 +26,16 @@ def welcome(country):
 
 def enterGuess():
     guess = input('Guess a letter: ')
-    guessedLetters.append(guess)
     return guess
     
-def checkGuess(guess, country):
+def checkForCorrectGuess(guess, country):
     for i in range(len(country)):
-        x = country[i]
-        if x == guess:
+        if country[i] == guess:
             print('Your guess is correct!')
-        elif x != guess:
-            print('Your guess is incorrect.')
-        else:
-            print('You already guessed that.')
         
-def updateGuesses():
+def updateGuesses(guess):
+    global guessedLetters
+    guessedLetters.append(guess)
     global guessesLeft
     guessesLeft -= 1
     # fill in guessed letters
@@ -42,5 +44,7 @@ def displayGuesses():
     print('You have guessed: ')
     print(guessedLetters)
     print('You have ' + str(guessesLeft) + ' guesses left.')
+    
+# something to display progress in word - will probably help with ending the game when the whole word is guessed
     
 runGame()
